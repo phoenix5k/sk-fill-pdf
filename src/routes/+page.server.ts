684b85947863527eaa44bd37125396e3ce8e01f5
Phoenix5k <me@phoenixlabs.gay>
2023-02-23 11:06:02 +0100

    reee

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,5 @@
 import { prisma } from '$lib/prisma';
 import type { PageServerLoad } from './$types';
-import blankPdf from '$lib/blank.pdf';
 
 export const load = (async () => {
 	const establishments = prisma.establishment.findMany();
